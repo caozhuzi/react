eac24d134c276383bcf969d8e9666bff157cf24c
caozhuzi <1713023757@qq.com>
2019-03-11 18:13:36 +0800

    加个注释

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          /* 加个注释 */
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
